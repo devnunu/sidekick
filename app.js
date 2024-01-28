5be26f96606a145b1e3b677c5f4f05fa1605e0b0
eunwoo <aufcl4858@naver.com>
2024-01-28 22:55:27 +0900

    주소 업데이트

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ app.message('hello', async ({ message, say }) => {
   await say(`Hey there <@${message.user}>!`);
 });
 
+// 슬랙 채널에 메시지 보내는 함수
+async function sendSlackMessage(channelId, message) {
+    try {
+      await app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: channelId,
+        text: message
+      });
+      console.log('Message sent successfully');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  }
+  
+
 (async () => {
   // Start your app
   await app.start();
